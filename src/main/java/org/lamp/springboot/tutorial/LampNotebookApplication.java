ccefadaafae90350bd8b90bd014e1b182bb34a07
Jeff Duan <jeff.duan@sap.com>
2022-12-19 17:53:37 +0800

    init project

---
+++ src/main/java/org/lamp/springboot/tutorial/LampNotebookApplication.java
@@ -0,0 +1,13 @@
+package org.lamp.springboot.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LampNotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LampNotebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lamp/springboot/tutorial/LampNotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lamp.springboot.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LampNotebookApplicationTests {
+
+	@Test
+	void contextLoads() throws InterruptedException {
+	}
+
+}
